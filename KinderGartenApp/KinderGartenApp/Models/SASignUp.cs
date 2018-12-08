d40271c8a10068f47dbda8691fb99acc9ba4a260
Anmol Tariq <anmoltariqbutt@gmail.com>
2018-12-08 22:12:35 +0500

    project

---
+++ KinderGartenApp/KinderGartenApp/Models/SASignUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KinderGartenApp.Models
+{
+    public class SASignUp
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string UserID { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [RegularExpression("([a-z]|[A-Z]|[0-9]|[\\W]){4}[a-zA-Z0-9\\W]{3,11}", ErrorMessage = "Invalid password format")]
+        [MinLength(8, ErrorMessage = "Minimum length of 8 characters is required")]
+        public string Password { get; set; }
+
+        [NotMapped] // Does not effect with database
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        
+
+    }
+}
+
